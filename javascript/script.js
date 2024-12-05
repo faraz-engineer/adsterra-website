4c435895679d3926ff71b9c5d55229ce8770f62a
Faraz <me.farazzafar@gmail.com>
2024-12-05 13:23:11 +0500

    publisher page continue

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,16 @@ buttons.forEach((button) => {
     button.classList.add("active");
   });
 });
+
+const links = document.querySelectorAll('.button-group a');
+
+    // Add click event listener to each link
+    links.forEach(link => {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        links.forEach(item => item.classList.remove('active'));
+
+        this.classList.add('active');
+      });
+    });
