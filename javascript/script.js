0f25208ff50a48dc1838e555c65a945baa93a2f2
Faraz <me.farazzafar@gmail.com>
2024-12-08 12:48:31 +0500

    navigation hamburger completed

--- javascript/script.js
+++ javascript/script.js
@@ -9,17 +9,36 @@ buttons.forEach((button) => {
 
 const links = document.querySelectorAll('.button-group a');
 
-    // Add click event listener to each link
-    links.forEach(link => {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        links.forEach(item => item.classList.remove('active'));
+// Add click event listener to each link
+links.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
 
-        this.classList.add('active');
+    links.forEach(item => item.classList.remove('active'));
+
+    this.classList.add('active');
+  });
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navbar.classList.toggle("active");
+})
+
+document.querySelectorAll("").forEach(n => n.
+addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navbar.classList.remove("active");
+}))
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
       });
-    });
-
-
-
-    
+  });
+});
